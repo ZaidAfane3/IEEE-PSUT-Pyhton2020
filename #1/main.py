7fc95dcd95a190331723a87c8ef78dda4f592966
Zaid Afaneh <afaneh.zaid98@gmail.com>
2020-04-13 11:46:33 +0300

    Initial Commit - 1st Lecture

---
+++ #1/main.py
@@ -0,0 +1,37 @@
+print ("Hello World ")
+print ("Hello World \n")
+age = 21
+for_my_bd = 5 
+print ("my age is %d my birthday is in %d months" %(age,for_my_bd))
+print ("my age is" , age , "my birthday is in", for_my_bd, "months")
+
+day = 12 
+month = 4 
+year = 2020 
+print (day,month,year, sep="/") # 12/4/2020
+name = "Zaid"
+print ("Hello " + "my name is " + name)
+email= '''
+Dear {1}{0},
+":D"
+'-_-'
+I hope my email finds you well, i have nothing tbh
+Goodbye {0}
+'''
+print (email.format("Zaid", "Mr."))
+print ((name+" ")*3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
