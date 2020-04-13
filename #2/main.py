8d25b83a29be8566da94eff79babeb48a06448f7
Zaid Afaneh <afaneh.zaid98@gmail.com>
2020-04-14 00:15:25 +0300

    Second Lecture

---
+++ #2/main.py
@@ -0,0 +1,128 @@
+# #print ("2" + 5)
+# print (2 + 5)
+
+# print (1_250_000)
+# print (1250000)
+
+# print (5)
+# print (+5)
+# print (-5)
+
+# print (83)
+# print (0b01010011)
+# print (0o123) 
+# print (0x53) # for memory usage 
+
+# print (0x53 + 0x53)
+# print (0x53 * 2)
+# print (0b01010011 + 2)
+# print (0b01010011 + 0xff)
+
+##########
+
+# print (3.0)
+# print (3.)
+
+# print (10/5)
+# print (10/5.0)
+# print (10.0/5)
+# print (10.0/5.0)
+
+# print (10//5)
+# print (6//4)
+# print (6/-4) 
+# print (6//-4) 
+
+# print (0.5)
+# print (.5)
+
+# var = 9.8e5 
+# var_negative = 9.8e-5 
+
+# print (var)
+# print (var_negative)
+# ############### 
+
+# print ("Hello", end = " :D \n")
+# print ('Hello' , "Wolrd" , sep="  --  ")
+# print ("""
+# Hello
+# """)
+
+# print (""" 'the wise man once said "bla bla bla bla"' """)
+# print ("the wise man once said \"bla bla bla bla\" ")
+
+
+# # < > == != and or 
+# print ( 3 > 5 )
+
+
+# print(True > False)
+# print(True < False)
+
+######### 
+
+#isinstance()  # True Flase
+#type()        # Data type (class)
+# integer = 10.5
+# string = "Hello"
+# print (type(integer))
+# print (type(string))
+# print (type(1>3))
+# print (type(0x53))
+# print (isinstance(integer, float))
+
+# str_1 = ""
+# str_2 = ''
+# str_3 = """ """
+# str_4 = ''' ''' 
+
+# print (type(str_1) , type(str_2) , type(str_3) , type(str_4) , sep="\n")
+# print (isinstance(str_1 , str))
+
+###########
+
+print (2 * 2 * 2)
+print (2 ** 3)
+print (0x4 ** 2)
+
+
+# 1234 -> 4321 
+
+print (11 // 10)
+print (11/10)
+print (114/100)
+print (114%100)
+
+
+
+num1 = 1234 
+alf = num1 % 10 # 4 
+num1 //= 10  # 123
+mi2at = num1  % 10 # 3 
+num1 //= 10  # 12 
+asharat = num1  % 10 # 2  
+num1 //= 10  # 1
+a7ad = num1 % 10 # 1  
+num2 = alf * 1000 + mi2at * 100 + asharat * 10 + a7ad * 1
+print (num1)
+print (num2)
+
+####### right-sided binding 
+#print (0x66666666666666666666666666666666666666666666666666666666666666666666666666666)
+#exit (0)
+print (3 ** 2 ** 4)
+# 43,046,721 
+# 6,561
+
+
+
+
+
+
+
+
+
+
+
+
